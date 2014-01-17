d9cebc0534d405d5ab42098d5235bd59986c3ce8
Ryan <ryan@Ryan-Morans-Macbook-Pro.local>
2014-01-17 10:03:45 -0500

    Initial Commit

---
+++ Quiz2/QuizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizAppDelegate.h
+//  Quiz2
+//
+//  Created by Ryan on 1/17/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz2/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz2
+//
+//  Created by Ryan on 1/17/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
